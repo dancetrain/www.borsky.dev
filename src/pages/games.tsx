abdf8d6c51b1efd064b4a86ad34c0fdc25d95f80
Pavel Borskyシ <dancetrain@users.noreply.github.com>
2023-03-12 13:49:34 +0100

    MasterMind: Added link to Wikipedia Page
    Removed Cubes from the games list

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -3,16 +3,15 @@ import Layout from "../components/Layout";
 import { Link } from "gatsby";
 
 const Games: React.FC = () => {
-  return <Layout mainStyles={{paddingTop: "1rem", marginBottom: "auto"}}>
-    Games
-    <ul>
-      <li>
-        <Link to={"/games/cubes"}>ThreeJS flying Cubes</Link>
-      </li>
-      <li>
-        <Link to={"/games/mastermind"}>Master Mind</Link>
-      </li>
-    </ul>
-  </Layout>
+  return (
+    <Layout mainStyles={{ paddingTop: "1rem", marginBottom: "auto" }}>
+      Games
+      <ul>
+        <li>
+          <Link to={"/games/mastermind"}>Master Mind</Link>
+        </li>
+      </ul>
+    </Layout>
+  );
 };
 export default Games;

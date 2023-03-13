3194caee43caf1b3d5e76c0bd78f1c0453e67c54
Pavel Borskyシ <dancetrain@users.noreply.github.com>
2023-03-13 20:55:48 +0100

    UI: Added link to home page to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 import TopNavMenu from "./TopNavMenu";
+import { Link } from "gatsby";
 
 type HeaderProps = {
-  style?: React.CSSProperties
-}
+  style?: React.CSSProperties;
+};
 
 const headerStyles: React.CSSProperties = {
   display: "flex",
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  padding: '0 2rem',
-}
+  flexDirection: "row",
+  justifyContent: "space-between",
+  padding: "0 2rem",
+};
 
 const divStyle: React.CSSProperties = {
-  lineHeight: '3rem',
-}
+  lineHeight: "3rem",
+};
 
 const headerDark: React.CSSProperties = {
   background: "#343a40",
-  color: "#e7e7e7"
-}
+  color: "#e7e7e7",
+};
 
-const Header: React.FC<HeaderProps> = ({style}) => {
+const Header: React.FC<HeaderProps> = ({ style }) => {
   return (
-      <header style={{...headerStyles,...headerDark, ...style}}>
-        <div style={divStyle}>
-          www.borsky.dev
-        </div>
-        <div>
-          <TopNavMenu style={divStyle}/>
-        </div>
-      </header>
-  )
+    <header style={{ ...headerStyles, ...headerDark, ...style }}>
+      <div style={divStyle}>
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          Pavel Borsky
+        </Link>
+      </div>
+      <div>
+        <TopNavMenu style={divStyle} />
+      </div>
+    </header>
+  );
 };
 export default Header;

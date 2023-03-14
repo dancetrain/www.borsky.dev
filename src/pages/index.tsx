024bbb89abf769d8766126fcaa741290342544e0
Pavel Borskyシ <dancetrain@users.noreply.github.com>
2023-03-14 08:06:35 +0100

    data: Changed link text for skills / cv page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const HomePage: React.FC = () => {
   return <Layout mainStyles={{paddingTop: '1rem', marginBottom: 'auto', fontSize: '30px'}}>
       <p>Hey! My name is Pavel</p>
       <p>I'm a Software Engineer based in Barcelona, Spain with more 12 years of commercial development <small>My first HTML courses I've finished in 1998</small></p>
-      <p>Currently looking for new opportunities, you can find my <Link to={"cv"}>CV here</Link></p>
+      <p>Currently looking for new opportunities, you can find my <Link to={"cv"}>Skills / CV here</Link></p>
   </Layout>
 }
 
